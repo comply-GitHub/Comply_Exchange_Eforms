edaeca7268f9a08fbacbb64da4bbbd98b836dde4
priyanshi1001 <priyanshi.nigam@mail.vinove.com>
2024-03-29 17:02:23 +0530

    issues

--- src/Components/W-8ECI/TaxPayer/index.tsx
+++ src/Components/W-8ECI/TaxPayer/index.tsx
@@ -179,7 +179,7 @@ export default function Tin(props: any) {
             </div>
           </div>
         </div>
-        <div className="row w-100 h-100">
+        <div className="row w-100">
           <div className="col-4">
             <div
               style={{ padding: "20px 0px", height: "100%" }}
